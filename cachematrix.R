eca28301af9a2c80bef9cf70a29317bb8d0a2f37
aniket557 <aniket557@gmail.com>
2015-01-24 14:27:38 +0530

    commit by Aniket for Pgm Assigment 2....draft

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function() x
+  setInverse <- function(solve) inverse <<- solve
+  getInverse <- function() inverse
+  list(set = set, getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$getMatrix()
+  inverse = {
+    if(det(data)==0)
+      stop("Matrix entered is not a square matrix, try again!!\n")
+    return(solve(data))
+  } 
+  
+  x$setInverse(inverse)
+  inverse
 }
